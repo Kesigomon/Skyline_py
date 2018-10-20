bf98c2a4ab6d17c8dd9058cf9458508d9b50ae73
Kesigomon <kesigomon@gmail.com>
2018-10-21 01:53:17 +0900

    agreeコマンドをちょいと修正。

--- new_skiline.py
+++ new_skiline.py
@@ -141,18 +141,22 @@ class Normal_Command:
 
     @commands.command()
     async def agree(self, ctx):
-        # 送信する文章指定。
-        content = """
+        normal_user = ctx.guild.get_role(268352600108171274)
+        if normal_user not in ctx.author.roles:
+            # 送信する文章指定。
+            content = """
 {0}さんの
 アカウントが登録されました！{1}の
 {2}個のチャンネルが利用できます！
 まずは<#437110659520528395>で自己紹介をしてみてください！
 """.format(ctx.author.mention, ctx.guild.name, len(ctx.guild.channels))
-# 左から順に、ユーザーのメンション、サーバーの名前、サーバーのチャンネル数に置き換える。
-        # 役職付与
-        await ctx.author.add_roles(ctx.guild.get_role(268352600108171274))
-        # メッセージ送信
-        await ctx.send(content)
+            # 左から順に、ユーザーのメンション、サーバーの名前、サーバーのチャンネル数に置き換える。
+            # 役職付与
+            await ctx.author.add_roles(normal_user)
+            # メッセージ送信
+            await ctx.send(content)
+        else:
+            await ctx.send('登録終わってますやんか')
 
 
 class Bot_Owner_Command:
