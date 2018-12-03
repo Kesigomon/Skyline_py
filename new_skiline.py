ce01500d3ebb54c015070c0ebe393b217e28d043
Kesigomon <kesigomon@gmail.com>
2018-12-03 20:55:59 +0900

    エマージェンシーコールを追加

--- new_skiline.py
+++ new_skiline.py
@@ -1068,7 +1068,7 @@ client.add_cog(DM_Command(client, 'DM用コマンド'))
 client.add_cog(Joke_Command(client, data, 'ネタコマンド'))
 client.add_cog(Role_panel(client, 449185870684356608, '役職パネル'))
 client.add_cog(Manage_channel(client, '自由チャンネル編集コマンド'))
-# client.add_cog(Emergency_call(client, '緊急呼び出しコマンド'))
+client.add_cog(Emergency_call(client, '緊急呼び出しコマンド'))
 client.add_cog(Categor_recover(client))
 if __name__ == '__main__':
     token = ''
