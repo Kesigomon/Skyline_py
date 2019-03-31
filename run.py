462b9311a3453808d48ccfaf57798f2e61231052
Kesigomon <kesigomon@gmail.com>
2019-03-31 21:35:30 +0900

    BOT実行用のソースを修正

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
+import asyncio
 import os
 from new_skyline2 import SKYLINE
 token = os.environ['token']
-client = SKYLINE()
-client.run(token)
+loop = asyncio.get_event_loop()
+client = SKYLINE(loop=loop)
+
+
+async def main():
+    await client.start(token)
+    await client.close()
+    all_tasks = asyncio.all_tasks(loop=loop)
+    while True:
+        done, pending = await asyncio.wait(list(all_tasks), timeout=5)
+        [t.cancel() for t in pending]
+        if not pending:
+            break
+
+main_task = loop.create_task(main())
+loop.run_until_complete(main_task)
+loop.close()
