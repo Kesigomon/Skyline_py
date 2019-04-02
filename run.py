c3edb698612e93df794f62a97bab7277d3804789
Kesigomon <kesigomon@gmail.com>
2019-04-03 07:29:35 +0900

    Disboard bumpなどタスクを復旧

--- run.py
+++ run.py
@@ -1,23 +1,27 @@
 import asyncio
 import os
+import datetime
 from new_skyline2 import SKYLINE
 token = os.environ['token']
-while True:
-    loop = asyncio.new_event_loop()
-    client = SKYLINE(loop=loop)
+loop = asyncio.new_event_loop()
+client = SKYLINE(loop=loop)
 
 
-    async def main():
-        await client.start(token)
-        await client.close()
-        all_tasks = [t for t in asyncio.all_tasks(loop=loop) if t != main_task]
-        while all_tasks:
-            done, pending = await asyncio.wait(all_tasks, timeout=5)
-            print(pending)
-            [t.cancel() for t in pending]
-            if not pending:
-                break
+async def main():
+    now = datetime.datetime.utcnow()
+    endtime = now.replace(hour=17, minute=0, second=0, microsecond=0)
+    if now >= endtime:
+        endtime += datetime.timedelta(days=1)
+    await asyncio.wait([client.start(token)], timeout=(endtime - now).total_seconds())
+    await client.close()
+    all_tasks = [t for t in asyncio.all_tasks(loop=loop) if t != main_task]
+    while all_tasks:
+        done, pending = await asyncio.wait(all_tasks, timeout=5)
+        print(pending)
+        [t.cancel() for t in pending]
+        if not pending:
+            break
 
-    main_task = loop.create_task(main())
-    loop.run_until_complete(main_task)
-    loop.close()
+main_task = loop.create_task(main())
+loop.run_until_complete(main_task)
+loop.close()
