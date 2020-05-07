c272069284f6e591b25f34e08624c39fe60d02b1
Kesigomon <kesigomon@gmail.com>
2020-05-08 07:27:23 +0900

    役職パネル機能をオミット

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,23 +1,22 @@
-from .normal import Normal_Command
-from .freecategory import FreeCategory
-from .staff import Staff_Command
-from .level import Level
-from .events import Events
+from .board import DiscussionBoard
+from .botowner import Bot_Owner_Command
 from .categoryindex import Category_Index
 from .categoryrecover import Category_recover
-from .botowner import Bot_Owner_Command
-from .rolepanel import Role_panel
-from .board import DiscussionBoard
-from .rolesave import Role_save
+from .events import Events
+from .freecategory import FreeCategory
+from .level import Level
 from .message_count import Message_count
+from .normal import Normal_Command
+from .rolesave import Role_save
+from .staff import Staff_Command
 from .ticket import Ticket
+
 
 def cogs(bot):
     bot.add_cog(Normal_Command(bot, '普通のコマンド'))
     bot.add_cog(Category_Index(bot, 'カテゴリインデックス'))
     bot.add_cog(Bot_Owner_Command(bot, 'BOTオーナー用コマンド'))
     bot.add_cog(Staff_Command(bot, 'スタッフ用コマンド'))
-    bot.add_cog(Role_panel(bot, '役職パネル'))
     bot.add_cog(FreeCategory(bot, '自由チャンネル編集コマンド'))
     bot.add_cog(Category_recover(bot, 'カテゴリーリカバリー'))
     bot.add_cog(Events(bot, '参加・退出通知、VC通知'))
